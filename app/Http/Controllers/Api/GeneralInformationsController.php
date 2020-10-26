be3bf00b4c63d10dccad76561636fc165ca6727e
Élie Meignan <contact@eliepse.fr>
2020-10-26 17:39:45 +0100

    feat: add general infos API with average datas

---
+++ app/Http/Controllers/Api/GeneralInformationsController.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace App\Http\Controllers\Api;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+
+class GeneralInformationsController
+{
+    public function __invoke(Request $request, string $country = null)
+    {
+        $query = DB::table("cases");
+
+        if (!empty($country = trim($country))) {
+            $query->where("country", $country);
+        }
+
+
+        $new_cases = $query->clone()->groupBy("reported_at")
+            ->selectRaw("SUM(new_cases) AS new_cases, reported_at")
+            ->get()
+            ->each(fn($val) => $val->new_cases = intval($val->new_cases));
+
+        $smooth = $this->getAverage($new_cases, 3);
+        $smoother = $this->getAverage($smooth, 3);
+
+        return [
+            "total" => [
+                "cases" => intval($query->sum("new_cases")),
+            ],
+            "data" => [
+                "raw" => $new_cases->pluck("new_cases", "reported_at"),
+                "smooth" => $smooth->pluck("new_cases", "reported_at"),
+                "smoother" => $smoother->pluck("new_cases", "reported_at"),
+            ]
+        ];
+    }
+
+    private function getAverage(Collection $data, int $days_around = 3): Collection
+    {
+        $smoothed = collect();
+        $count = $data->count();
+
+        for ($i = 0; $i < $count; $i++) {
+            $range = [$i - $days_around, $i + $days_around];
+            $buffer = [];
+            for ($j = $range[0]; $j < $range[1]; $j++) {
+                if ($cases = $data[$j]->new_cases ?? null) {
+                    $buffer[] = $cases;
+                }
+            }
+            $day = clone $data[$i];
+            $day->new_cases = round(count($buffer) === 0 ? 0 : array_sum($buffer) / count($buffer));
+            $smoothed[$i] = $day;
+        }
+
+        return $smoothed;
+    }
+}
